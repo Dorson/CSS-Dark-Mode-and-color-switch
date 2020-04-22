3408781ca4aae791956f81ac0e6268c561749499
Nik K <4200435+Dorson@users.noreply.github.com>
2020-04-22 08:12:57 +0200

    harden memory recall logic to be fail-safe
    Hardened the CSS class memory recall function against critical inpupt errors in exceptional cases.
    
    One could never know if this code will run in the apps or hardware for critical life support.
    Or maybe inside of machines that need to work 100% all the time to support safety of humans.
    
    
    Now the functon exits the loop, if any errors of the input variables are found.
    Limits possible attack vectors, where safety is an issue.
    
    
    If you want to test the function for critical errors or with false data input,
    use those console log variables for testing.
    
    Fake the input data and see what happens in the console logs.
    
    
       console.log( "load state was: " + document.readyState ) ;
    
       console.log( "Tag : " + Tag ) ;
    
       console.log( "TagNotAlive : " + TagNotAlive ) ;
    
       console.log( "MemoryName : " + MemoryName ) ;
    
       console.log( "MemoryValue : " + MemoryValue ) ;

--- JS-CSS-Class-Settings-Recall-on-load.js
+++ JS-CSS-Class-Settings-Recall-on-load.js
@@ -1,6 +1,6 @@
 /*
 -::: JavaScript CSS Class Memory Recall function for websites and HTML apps.
---- Original author	:: Nik K. linkedin.com/in/nik--k---/   (  4.Feb 2020). 
+--- Original author :: Nik K. linkedin.com/in/nik--k---/   (  4.Feb 2020). 
 --- Code adoption   :: YourName  (date).
 --- License         :: GNU General Public License v 3.0.
 --- Latest version  :: github.com/Dorson/CSS-Dark-Mode-and-color-switch
@@ -9,30 +9,48 @@
 window.onerror = null ;
 
 
-/* During load time, we remember the saved CSS classes from local memory */
+/*
+  During load time, we remember the saved CSS classes from localStorage
+  Only works with HTML Tags that are already loaded before the script run !
+  Optimally the HTML tag. Works for the first Tag of it's type only !
+*/
 
-document.addEventListener("DOMContentLoaded", RememberClasses() , true ) ;
+document.addEventListener( 'DOMContentLoaded' , function() {
+
+  RememberClasses() ;
+
+});
+
 
 function RememberClasses() {
 
- var ClassMemory = [
+ const ClassMemory = [
       ["html" , "ColorMode"] ,
       ["html" , "TextSize"] 
    ] ;
 
- for ( let [ Tag, MemoryName ] of ClassMemory ) {
-    /*  On Errors we skip one loop. */
-    if (!localStorage.getItem( MemoryName ) || !document.querySelector( Tag ) ) {
+
+ for ( let [ Tag , MemoryName ] of ClassMemory ) {
+
+    let TagNotAlive = !(document.querySelector( Tag )) ;
+
+    let MemoryValue = window.localStorage.getItem( MemoryName ) ;
+
+    /*  On logic Errors we skip one loop. */
+
+    if ( !MemoryValue || TagNotAlive ) {
        continue ;
      }
-     else {
+
     /* Else we remember and set CSS classes. */
-        var MemoryValue = localStorage.getItem( MemoryName );
+     else {
+
         document.querySelector( Tag ).classList.add( MemoryValue );
       }
   }
 
 }
+
 
 
 
